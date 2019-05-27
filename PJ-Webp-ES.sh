327c871286cb159893107d0c524e5723acaf50b2
H4r0s <47732620+H4r0s@users.noreply.github.com>
2019-05-26 23:36:06 -0300

    Update PJ-Webp-ES.sh

--- PJ-Webp-ES.sh
+++ PJ-Webp-ES.sh
@@ -5,49 +5,49 @@ function _menu()
   echo 
   echo "Selecciona una opcion:"
   echo
-  echo "1) Convert JPG to WEBP"
-  echo "2) Convert JPEG to WEBP"
-  echo "3) Convert PNG to WEBP"
-  echo "4) Convert JPG to WEBP and Delete JPG files"
-  echo "5) Convert JPEG to WEBP and Delete JPEG files"
-  echo "6) Convert PNG to Webp and Delete PNG files"
-  echo "7) Exit"
+  echo "1) Convertit JPG a WEBP"
+  echo "2) Convertit JPEG a WEBP"
+  echo "3) Convertit PNG a WEBP"
+  echo "4) Convertit JPG a WEBP y borrar archivos JPG"
+  echo "5) Convertit JPEG a WEBP y borrar archivos JPEG"
+  echo "6) Convertit PNG a WEBP y borrar archivos PNG"
+  echo "7) Salir"
 }
 opc=0
  until [ $opc -eq 7 ]
 do
   case $opc in
     1)
-      echo "type the path to the working folder"
+      echo "Teclee el nombre de la carpeta a trabajar"
       read H
       mogrify -format webp "$H/*.[jJ][pP][gG]"
       ;;
     2)
-      echo "type the path to the working folder"
+      echo "Teclee el nombre de la carpeta a trabajar"
       read H
       mogrify -format webp "$H/*.[jJ][pP][eE][gG]"
       ;;
     3)
-      echo "type the path to the working folder"
+      echo "Teclee el nombre de la carpeta a trabajar"
       read H
       mogrify -format webp "$H/*.[pP][nN][gG]"
       ;;
     4)
-      echo "type the path to the working folder"
+      echo "Teclee el nombre de la carpeta a trabajar"
       read H 
       mogrify -format webp "$H/*.[jJ][pP][gG]"
       mv "$H/*.[jJ][pP][gG]" /tmp
       rm -rf /tmp/*.[jJ][pP][gG]
       ;;
     5)
-      echo "type the path to the working folder"
+      echo "Teclee el nombre de la carpeta a trabajar"
       read H  
       mogrify -format webp "$H/*.[jJ][pP][eE][gG]"
       mv "$H/*.[jJ][pP][eE][gG]" /tmp
       rm -rf /tmp/*.[jJ][pP][eE][gG]    
       ;;
     6)
-      echo "type the path to the working folder"
+      echo "Teclee el nombre de la carpeta a trabajar"
       read H  
       mogrify -format webp "/tmp/*.[pP][nN][gG]"
       mv "$H/*.[pP][nN][gG]" /tmp
