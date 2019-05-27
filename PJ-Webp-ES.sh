940b1bde067e856a77668aa9c489249db5b7627e
H4r0s <47732620+H4r0s@users.noreply.github.com>
2019-05-26 22:51:02 -0300

    Create PJ-Webp-ES.sh
    This script can convert jpg, jpeg and png to webp

---
+++ PJ-Webp-ES.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#H4r0s
+function _menu()
+{
+  echo 
+  echo "Selecciona una opcion:"
+  echo
+  echo "1) Convert JPG to WEBP"
+  echo "2) Convert JPEG to WEBP"
+  echo "3) Convert PNG to WEBP"
+  echo "4) Convert JPG to WEBP and Delete JPG files"
+  echo "5) Convert JPEG to WEBP and Delete JPEG files"
+  echo "6) Convert PNG to Webp and Delete PNG files"
+  echo "7) Exit"
+}
+opc=0
+ until [ $opc -eq 7 ]
+do
+  case $opc in
+    1)
+      echo "type the path to the working folder"
+      read H
+      mogrify -format webp "$H/*.[jJ][pP][gG]"
+      ;;
+    2)
+      echo "type the path to the working folder"
+      read H
+      mogrify -format webp "$H/*.[jJ][pP][eE][gG]"
+      ;;
+    3)
+      echo "type the path to the working folder"
+      read H
+      mogrify -format webp "$H/*.[pP][nN][gG]"
+      ;;
+    4)
+      echo "type the path to the working folder"
+      read H 
+      mogrify -format webp "$H/*.[jJ][pP][gG]"
+      mv "$H/*.[jJ][pP][gG]" /tmp
+      rm -rf /tmp/*.[jJ][pP][gG]
+      ;;
+    5)
+      echo "type the path to the working folder"
+      read H  
+      mogrify -format webp "$H/*.[jJ][pP][eE][gG]"
+      mv "$H/*.[jJ][pP][eE][gG]" /tmp
+      rm -rf /tmp/*.[jJ][pP][eE][gG]    
+      ;;
+    6)
+      echo "type the path to the working folder"
+      read H  
+      mogrify -format webp "/tmp/*.[pP][nN][gG]"
+      mv "$H/*.[pP][nN][gG]" /tmp
+      rm -rf /tmp/*.[pP][nN][gG]
+      ;;
+    *) 
+      _menu
+      ;;
+    )
+  esac
+  read opc
+done
